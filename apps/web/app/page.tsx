ac8bb24c4197d6bc97c3921c4543e3acebdf29bf
chrisshaddad <chriss.hadad@gmail.com>
2025-12-30 21:38:04 +0200

    web: integrate shadcn components with absolute path support

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@/components/button";
+import { Button } from "@/ui/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -63,9 +63,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
+        <Button className={styles.secondary}>Open alert</Button>
         <h1 className="text-3xl font-bold underline">Some Tailwind CSS</h1>
       </main>
       <footer className={styles.footer}>

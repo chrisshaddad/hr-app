1c20d33fc6de19c39144946b9a3e0723b6a710b4
chrisshaddad <chriss.hadad@gmail.com>
2025-12-30 23:28:05 +0200

    api: add bull mq module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        url: process.env.REDIS_URL,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

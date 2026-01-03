0377880a70657b8074941aa384169ac90107402e
chrisshaddad <chriss.hadad@gmail.com>
2026-01-04 00:55:40 +0200

    api: init auth controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
         url: process.env.REDIS_URL,
       },
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
